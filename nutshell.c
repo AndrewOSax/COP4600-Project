a6a69ad3dfe94aeb6f754fdacb60bb56a8ddc670
AndrewOSax <60533458+AndrewOSax@users.noreply.github.com>
2021-04-02 15:52:02 -0400

    Update nutshell.c

--- nutshell.c
+++ nutshell.c
@@ -1,41 +1,52 @@
 #include <stdio.h>
-#include "nutshscanner.h"
-int yylex();
-extern char* yytext;
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <limits.h>
+#include "global.h"
+extern int yyparse();
+char *getcwd(char *buf, size_t size);
 
-void print_line(int line){
-	printf("%4d | ",line);
-}
+void init(){
+	aliasIndex = 0;
+    varIndex = 0;
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+
+    strcpy(varTable.var[varIndex], "PWD");
+    strcpy(varTable.val[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "HOME");
+    strcpy(varTable.val[varIndex], cwd);
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PROMPT");
+    strcpy(varTable.val[varIndex], "nutshell");
+    varIndex++;
+    strcpy(varTable.var[varIndex], "PATH");
+    strcpy(varTable.val[varIndex], ".:/bin");
+    varIndex++;
+
+    strcpy(aliasTable.name[aliasIndex], ".");
+    strcpy(aliasTable.val[aliasIndex], cwd);
+    aliasIndex++;
+
+    char *pointer = strrchr(cwd, '/');
+    while(*pointer != '\0') {
+        *pointer ='\0';
+        pointer++;
+    }
+    strcpy(aliasTable.name[aliasIndex], "..");
+    strcpy(aliasTable.val[aliasIndex], cwd);
+    aliasIndex++;
+
+    system("clear");
+};
 
 int main(){
-	print_line(1);
-	
-	int line = 1;
-	
-	while (1){
-		int token = yylex();
-		if (token == 0){
-			break;
-		}
-		else{
-			printf("scanned");
-		}
-		/*if (token == STRING_LITERAL){
-			
-		}
-		else if (token == NUMBER_LITERAL){
-			
-		}
-		else if (token == BOOLEAN_LITERAL){
-			
-		}
-		else if (token == NULL_LITERAL){
-			
-		}
-		else{
-			
-		}*/
-	}
-	printf("\n");
+	init();
+	while(1){
+        printf("[%s]@%s>> ", varTable.val[2], varTable.val[0]);
+        yyparse();
+    }
 	return 0;
 }
