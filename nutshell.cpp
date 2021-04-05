df6a6f0b5cdcb8f777659327b67f814cb2cc547a
AndrewOSax <60533458+AndrewOSax@users.noreply.github.com>
2021-04-04 21:56:39 -0400

    Create nutshell.cpp

---
+++ nutshell.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <unistd.h>
+#include <limits.h>
+#include "global.h"
+#include <unordered_map>
+extern int yyparse();
+char *getcwd(char *buf, size_t size);
+std::unordered_map<std::string,std::string> varTable;
+std::unordered_map<std::string,std::string> aliasTable;
+bool firstWord;
+
+void init(){
+    char cwd[PATH_MAX];
+    getcwd(cwd, sizeof(cwd));
+	std::string pwd = std::string(cwd);
+	varTable.emplace("PWD",pwd);
+	varTable.emplace("HOME",pwd);
+    varTable.emplace("PROMPT","nutshell");
+	varTable.emplace("PATH",pwd+":/bin");
+
+	aliasTable.emplace(".",pwd);
+	int found = pwd.rfind("/");
+	if (found != -1){
+		aliasTable.emplace("..",pwd.substr(0,found));
+	}
+	else{
+		aliasTable.emplace("..","");
+	}
+	
+	firstWord = true;
+    system("clear");
+};
+
+int main(){
+	init();
+	while(1){
+        printf("[%s]@%s>> ", varTable["PROMPT"].c_str(), varTable["PWD"].c_str());
+        yyparse();
+    }
+	return 0;
+}
